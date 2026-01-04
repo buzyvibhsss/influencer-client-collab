3b4026d163d9d68eb3010ca8afdb5f8534a74e11
Vibhanshi Rajput <vibhanshirajput837@gmail.com>
2026-01-04 17:51:23 +0530

    Update server.js for Render deployment

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ let config = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     dateStrings: true
 }
 
